69e1b825d22e12f4355c18a217b3e485c45e6757
東都 彩希 <s_azuma@sharecrest.com>
2023-06-05 14:33:21 +0900

    Java入門 Lesson2 -Variable追加　gitにcommitしました

---
+++ java_02_02/src/com/java/lesson/DataOfInteger.java
@@ -0,0 +1,16 @@
+package com.java.lesson;
+
+public class DataOfInteger {
+	
+	public static void main(String[] args) {
+	int num1 = 123456;
+	long num2 = 1234567890;
+	short num3 = 123;
+	byte num4 = 1;
+	
+	System.out.println(num1);
+	System.out.println(num2);
+	System.out.println(num3);
+	System.out.println(num4);
+	}
+}
