28ffd00642d58212ae9be5f43de719496ff059d6
東都 彩希 <s_azuma@sharecrest.com>
2023-06-05 11:40:31 +0900

    Java入門 - Lesson1 -Lesson1追加:gitにcommitしました

---
+++ src/com/java/lesson/Lesson1.java
@@ -0,0 +1,12 @@
+package com.java.lesson;
+
+public class Lesson1 {
+	public static void main(String[] args) {
+		System.out.println("Java入門の練習問題を始めます。");
+		final int TEISU = 15; //定数TEISUを宣言し、15で初期化
+		int hensu = 10; //変数hensuを宣言し、10で初期化
+		hensu = 50; //変数hensuに50を代入
+		System.out.println(hensu); //変数hensuをコンソールに出力
+	}
+	
+}
