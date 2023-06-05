5aa42c4d58e2d76ffd5df1019b346d61c12fac51
東都 彩希 <s_azuma@sharecrest.com>
2023-06-05 10:39:33 +0900

    Java入門 -Lesson1 -Coments 追加:gitにcommitしました

---
+++ src/com/java/lesson/Coments.java
@@ -0,0 +1,14 @@
+package com.java.lesson;
+
+public class Coments {
+	/**Commentsクラス
+	 * このクラスは、コメントの書き方を学ぶためのクラスです。
+	 * @author s_azuma
+	 * @param 引数（パラメーター）についての説明
+	 * @return 戻り値についての説明
+	 * @throw 例外処理についての説明
+	 */
+	public static void main(String[] args) {
+		System.out.println("JavaDocを記載しました。");
+	}
+}
